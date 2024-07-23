fe0a5f03c85315ea7a29d1451a6eeb0152cbb478
Abraham <ricoabraham879@gmail.com>
2024-07-23 13:11:13 -0500

    Changing the Admin and User Dashboard

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>User dashboard</h1>
+    <main class="pt-90">
+        <div class="mb-4 pb-4"></div>
+        <section class="my-account container">
+            <h2 class="page-title">My Account</h2>
+            <div class="row">
+                <div class="col-lg-3">
+                    @include('user.account-nav')
+                </div>
+                <div class="col-lg-9">
+                    <div class="page-content my-account__dashboard">
+                        <p>Hola <strong>Usuario</strong></p>
+                        <p>Desde el panel de control de tu cuenta, puedes ver tus <a class="unerline-link"
+                                href="account_orders.html">pedidos
+                                recientes</a>, gestionar tus <a class="unerline-link" href="account_edit_address.html">direcciones de
+                                envío</a>, y <a class="unerline-link" href="account_edit.html">editar tu contraseña y detalles de la cuenta.</a></p>
+                    </div>                    
+                </div>
+            </div>
+        </section>
+    </main>
 @endsection
