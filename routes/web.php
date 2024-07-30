c8da5ec37a25ef42d48ef4f402709dc2fa6b8f8b
Abraham <ricoabraham879@gmail.com>
2024-07-29 20:41:09 -0500

    Admin Delete Category

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,7 @@ Route::middleware(['auth', AuthAdmin::class])->group(function() {
     Route::get('/admin/categories', [AdminController::class, 'categories'])->name('admin.categories');
     Route::get('/admin/categories/add', [AdminController::class, 'category_add'])->name('admin.category.add');
     Route::post('/admin/categories/store', [AdminController::class, 'category_store'])->name('admin.category.store');
+    Route::get('/admin/categories/{id}/edit', [AdminController::class, 'category_edit'])->name('admin.category.edit');
+    Route::put('/admin/categories/update', [AdminController::class, 'category_update'])->name('admin.category.update');
+    Route::delete('/admin/categories/{id}/delete', [AdminController::class, 'category_delete'])->name('admin.category.delete');
 });
